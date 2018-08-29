e59db545b4577383493fb5c3d170ed3022586738
Susumu Seino <seino@susu.mu>
2018-08-29 21:24:22 +0900

    fork from wp-instant-setup by miya

---
+++ README.md
@@ -0,0 +1,54 @@
+# WooCommerce Installer on your desktop.
+
+WooCommerce development environment with PHP built-in web server + WP-CLI.
+fork by [miya0001/wp-instant-setup: WordPress desktop installer](https://github.com/miya0001/wp-instant-setup)
+
+
+## Requires
+
+* OSX
+* php 5.4 or later
+* MySQL
+* wget
+
+### Recommend
+
+* [Mailcatcher](http://mailcatcher.me/)
+
+## Uage
+
+```
+$ curl https://.../run.sh | bash -s <db-name>
+```
+
+or
+
+```
+$ ./run.sh <db-name>
+```
+
+### Defaults
+
+* db-user: `root`
+* db-pass: (empty)
+* db-name: `woodev`
+
+## How to use
+
+```
+$ mkdir ~/Desktop/wordpress && cd $_
+$ curl https://raw.githubusercontent.com/musus/woo-instant-setup/master/run.sh | bash
+```
+
+Or
+
+```
+$ git clone git@github.com:musus/woo-instant-setup.git && cd wp-instant-setup
+$ ./run.sh
+```
+
+
+## Default Account
+
+* User: `admin`
+* Password: `admin`
---
+++ run.sh
@@ -0,0 +1,96 @@
+#!/usr/env bash
+
+set -ex;
+
+mysql.server start
+
+DB_USER="root"
+DB_PASS=""
+DB_NAME=${1-woodev}
+PORT=8080
+WP_PATH=$(pwd)/www
+WP_TITLE='Welcome to the WordPress'
+WP_DESC='Hello World!'
+WP_XML=$(pwd)/import.xml
+
+if [ -e "$WP_PATH/wp-config.php" ]; then
+    open http://127.0.0.1127:$PORT
+    wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
+    exit 0
+fi
+
+if ! wp --info ; then
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    rm -fr bin && mkdir bin
+    mv wp-cli.phar wp
+    chmod 755 wp
+fi
+
+echo "path: www" > $(pwd)/wp-cli.yml
+
+if [ $DB_PASS ]; then
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER -p$DB_PASS
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER -p$DB_PASS
+else
+    echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u$DB_USER
+    echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u$DB_USER
+fi
+
+wp core download --path=$WP_PATH --locale=ja --force
+
+if [ $DB_PASS ]; then
+wp core config \
+--dbhost=localhost \
+--dbname="$DB_NAME" \
+--dbuser="$DB_USER" \
+--dbpass="$DB_PASS" \
+--dbprefix=wp_ \
+--locale=ja \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+define( 'WP_MAILCATCHER', true );
+PHP
+else
+wp core config \
+--dbhost=localhost \
+--dbname=$DB_NAME \
+--dbuser=$DB_USER \
+--dbprefix=wp_ \
+--locale=ja \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+define( 'WP_MAILCATCHER', true );
+PHP
+fi
+
+wp core install \
+--url=http://127.0.0.1:$PORT \
+--title="WordPress" \
+--admin_user="admin" \
+--admin_password="admin" \
+--admin_email="admin@example.com"
+
+wp rewrite structure "/%category%/%post_id%"
+
+wp option update blogname "$WP_TITLE"
+wp option update blogdescription "$WP_DESC"
+
+if [ -e "provision-post.sh" ]; then
+    bash provision-post.sh
+fi
+
+wp plugin install show-current-template admin-bar-id-menu simply-show-ids duplicate-post wordpress-importer mailcatcher --activate
+wp plugin activate wp-multibyte-patch
+wp plugin delete akismet hello
+
+if [ -f ${WP_XML} ]; then
+    wp import --authors=create import.xml
+else
+    wget https://raw.githubusercontent.com/jawordpressorg/theme-test-data-ja/master/wordpress-theme-test-date-ja.xml
+    wp import --authors=create wordpress-theme-test-date-ja.xml
+fi
+
+open http://127.0.0.1:$PORT
+wp server --host=0.0.0.0 --port=$PORT --docroot=$WP_PATH
